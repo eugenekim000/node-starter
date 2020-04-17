0a7078bdd794a94b3c91eec376b901b7b7b49033
Justin Hewlett <justin-hewlett@pluralsight.com>
2020-04-17 09:55:02 -0400

    Initial commit

---
+++ server/src/course-api.js
@@ -0,0 +1,17 @@
+const axios = require('axios')
+
+async function get(courseId) {
+  try {
+    const res = await axios.get(`https://ep-coding-challenge-us-west-2.s3-us-west-2.amazonaws.com/courses/${courseId}`)
+
+    return { id: courseId, title: res.data.title }
+  } catch (err) {
+    console.log(`Error finding course '${courseId}' from API`, err)
+
+    return null
+  }
+}
+
+module.exports = {
+  get
+}
